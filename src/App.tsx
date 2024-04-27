4d36d29fe4962c6b1bab268df9409bf83b6731b8
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-04-27 16:11:43 -0500

    feat: add UI Button component with Tailwind CSS
    Introduced a new Button component under UI components. This component utilizes Tailwind CSS for styling, with various predefined variants and sizes. Added Tailwind CSS configuration files, and updated the global CSS with Tailwind directives for styling. Additionally, updated App.tsx to showcase the new Button component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import "./global.css";
+import { Button } from "@/components/ui/button.tsx";
+
 export function App() {
-  return <h1>Pizza Shop</h1>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
+  );
 }

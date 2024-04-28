f36a17480e10c9301e0dc3ac2843ed862b05644e
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-04-28 15:38:19 -0500

    feat: integrate react-helmet-async for dynamic titles
    In this commit, react-helmet-async has been integrated into the app to handle dynamic page titles. This provides a better user experience by clearly indicating the current page in the browser's title bar. Changes include adding the package to dependencies and using it in App.tsx, Dashboard.tsx and Sign-In.tsx. The base title of the website has also been updated in index.html.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./global.css";
 import { router } from "@/routes.tsx";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 }

32c7820d7642536dd073a22bb159a71152444592
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-05-17 16:42:31 -0500

    feat: add "Orders" page and implement necessary UI components
    This commit includes the addition of a new "Orders" page to the application and implementation of corresponding UI components. Additional changes include minor updates to the table, button components and adjustment of the application routing to include the new "Orders" page.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { AppLayout } from "@/pages/_layouts/app.tsx";
 import { AuthLayout } from "@/pages/_layouts/auth.tsx";
 import { Dashboard } from "@/pages/app/dashboard.tsx";
+import { Orders } from "@/pages/app/orders/orders.tsx";
 import { SignIn } from "@/pages/auth/sign-in.tsx";
 import { SignUp } from "@/pages/auth/sign-up.tsx";
 import { createBrowserRouter } from "react-router-dom";
@@ -13,6 +14,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Dashboard />,
+      },
+      {
+        path: "/orders",
+        element: <Orders />,
       },
     ],
   },

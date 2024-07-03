3c47f73cda480c448d3e2cfbf3296cbf25178c7b
Marcos Santos <marcosdiego.ns01@gmail.com>
2024-07-03 13:50:16 -0500

    feat: add order cancellation functionality
    Implemented the API call to cancel an order and updated the UI accordingly. This includes disabling the cancel button if the order status is not 'pending' or 'processing', and updating the order list cache after an order is successfully cancelled.

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -8,6 +8,9 @@ import { TableCell, TableRow } from "@/components/ui/table";
 import { OrderDetails } from "./order-details";
 import { OrderStatus } from "@/pages/app/orders/order-status.tsx";
 import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { cancelOrder } from "@/api/cancel-order.ts";
+import { GetOrdersResponse } from "@/api/get-orders.ts";
 
 interface OrderTableRowProps {
   order: {
@@ -21,6 +24,33 @@ interface OrderTableRowProps {
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
   const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: cancelOrderFn } = useMutation({
+    mutationFn: cancelOrder,
+    async onSuccess(_, { orderId }) {
+      const ordersListCache = queryClient.getQueriesData<GetOrdersResponse>({
+        queryKey: ["orders"],
+      });
+
+      ordersListCache.forEach(([cacheKey, cacheData]) => {
+        if (!cacheData) {
+          return;
+        }
+
+        queryClient.setQueryData<GetOrdersResponse>(cacheKey, {
+          ...cacheData,
+          orders: cacheData.orders.map((order) => {
+            if (order.orderId === orderId) {
+              return { ...order, status: "canceled" };
+            }
+
+            return order;
+          }),
+        });
+      });
+    },
+  });
 
   return (
     <TableRow>
@@ -66,7 +96,12 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
         </Button>
       </TableCell>
       <TableCell>
-        <Button variant="ghost" size="xs">
+        <Button
+          disabled={!["pending", "processing"].includes(order.status)}
+          onClick={() => cancelOrderFn({ orderId: order.orderId })}
+          variant="ghost"
+          size="xs"
+        >
           <X className="mr-2 h-3 w-3" />
           Cancelar
         </Button>

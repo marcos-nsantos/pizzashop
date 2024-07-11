9f7a2090bd5de513eb51ea36bbb652f56d357fe3
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-07-11 00:51:38 +0000

    feat: add dynamic dashboard metrics
    This commit introduces dynamic dashboard metrics by implementing API calls to fetch data for day and month orders amount, as well as month revenue and canceled orders amount. These changes ensure the dashboard metrics reflect live data and allow real-time monitoring.

--- src/pages/app/dashboard/month-canceled-orders-amount.tsx
+++ src/pages/app/dashboard/month-canceled-orders-amount.tsx
@@ -5,8 +5,15 @@ import {
   CardTitle,
 } from "@/components/ui/card.tsx";
 import { DollarSign } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import { getMonthCanceledOrdersAmount } from "@/api/get-month-canceled-orders-amount.ts";
 
 export function MonthCanceledOrdersAmount() {
+  const { data: monthCanceledOrdersAmount } = useQuery({
+    queryKey: ["metrics", "month-canceled-orders-amount"],
+    queryFn: getMonthCanceledOrdersAmount,
+  });
+
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -17,11 +24,30 @@ export function MonthCanceledOrdersAmount() {
       </CardHeader>
 
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">32</span>
-        <p className="text-xs text-muted-foreground">
-          <span className="text-emerald-500 dark:text-emerald-400">-2%</span> em
-          relação a ontem
-        </p>
+        {monthCanceledOrdersAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {monthCanceledOrdersAmount.amount.toLocaleString("pt-BR")}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {monthCanceledOrdersAmount.diffFromLastMonth < 0 ? (
+                <>
+                  <span className="text-emerald-500 dark:text-emerald-400">
+                    {monthCanceledOrdersAmount.diffFromLastMonth}%
+                  </span>{" "}
+                  em relação ao mês passado
+                </>
+              ) : (
+                <>
+                  <span className="text-rose-500 dark:text-rose-400">
+                    +{monthCanceledOrdersAmount.diffFromLastMonth}%
+                  </span>{" "}
+                  em relação ao mês passado
+                </>
+              )}
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   );

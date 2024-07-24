5f5bc02c0182b6b208b9fca1fc888984fe744fe2
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-07-23 19:05:21 -0500

    test: add test for NavLink component
    A new test has been added for the NavLink component. This test ensures that the NavLink component properly highlights the current page link.

---
+++ src/components/nav-link.spec.tsx
@@ -0,0 +1,24 @@
+import { NavLink } from "@/components/nav-link.tsx";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+
+describe("NavLink", () => {
+  it("should highlight the nav link when is the current page link", () => {
+    const wrapper = render(
+      <>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </>,
+      {
+        wrapper: ({ children }) => {
+          return (
+            <MemoryRouter initialEntries={["/about"]}>{children}</MemoryRouter>
+          );
+        },
+      },
+    );
+
+    expect(wrapper.getByText("About").dataset.current).toEqual("true");
+    expect(wrapper.getByText("Home").dataset.current).toEqual("false");
+  });
+});

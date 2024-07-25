e360053581d658fe67039ab3a6703605702d0be4
Marcos Santos <marcosdiego.ns01@gmail.com>
2024-07-25 13:23:00 -0500

    feat: add mock for restaurant registration
    This commit introduces a new mock endpoint for restaurant registration using MSW. It includes a condition to return a 201 status for "Pizza Shop" and a 400 status for other restaurant names. The mock is integrated into the existing setup for the MSW worker.

--- src/api/mocks/index.tsx
+++ src/api/mocks/index.tsx
@@ -1,8 +1,9 @@
 import { setupWorker } from "msw/browser";
 import { env } from "../../../env.ts";
 import { signInMock } from "@/api/mocks/sign-in-mock.tsx";
+import { registerRestaurantMock } from "@/api/mocks/register-restaurant-mock.ts";
 
-export const worker = setupWorker(signInMock);
+export const worker = setupWorker(signInMock, registerRestaurantMock);
 
 export async function enableMSW() {
   if (env.MODE !== "test") {

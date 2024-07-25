6b6eea42d541de2038652b0998a151ebd3430a57
Marcos Santos <marcosdiego.ns01@gmail.com>
2024-07-25 13:13:06 -0500

    test: add sign-in mock for API testing
    Introduced `.env.test` for test-specific environment settings. Added `sign-in-mock.tsx` for mocking the sign-in API endpoint. Updated the MSW worker setup to include the sign-in mock and adjusted the environment variable schema to accept non-URL strings for `VITE_API_URL`.

--- src/api/mocks/index.tsx
+++ src/api/mocks/index.tsx
@@ -1,7 +1,8 @@
 import { setupWorker } from "msw/browser";
 import { env } from "../../../env.ts";
+import { signInMock } from "@/api/mocks/sign-in-mock.tsx";
 
-export const worker = setupWorker();
+export const worker = setupWorker(signInMock);
 
 export async function enableMSW() {
   if (env.MODE !== "test") {

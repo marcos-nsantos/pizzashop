c52b68b7c2039425301b14619ecdba1fea334e4a
Marcos Santos <marcosdiego.ns01@gmail.com>
2024-07-25 13:32:14 -0500

    test: add new API mocks for various metrics
    This commit introduces mocks for day orders, month orders, month canceled orders, month revenue, daily revenue in a period, and popular products. These mocks are integrated into the existing mock service worker setup, enhancing local testing capabilities.

--- src/api/mocks/index.tsx
+++ src/api/mocks/index.tsx
@@ -2,8 +2,23 @@ import { setupWorker } from "msw/browser";
 import { env } from "../../../env.ts";
 import { signInMock } from "@/api/mocks/sign-in-mock.tsx";
 import { registerRestaurantMock } from "@/api/mocks/register-restaurant-mock.ts";
+import { getDayOrdersAmountMock } from "@/api/mocks/get-day-orders-amount-mock.ts";
+import { getMonthOrdersAmountMock } from "@/api/mocks/get-month-orders-amount-mock.ts";
+import { getMonthCanceledOrdersAmountMock } from "@/api/mocks/get-month-canceled-amount-mock.ts";
+import { getMonthRevenueMock } from "@/api/mocks/get-month-revenue-mock.ts";
+import { getDailyRevenueInPeriodMock } from "@/api/mocks/get-daily-revenue-in-period-mock.ts";
+import { getPopularProductsMock } from "@/api/mocks/get-popular-products-mock.ts";
 
-export const worker = setupWorker(signInMock, registerRestaurantMock);
+export const worker = setupWorker(
+  signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
+);
 
 export async function enableMSW() {
   if (env.MODE !== "test") {

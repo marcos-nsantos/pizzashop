d52b07eee7934cd0e65c5f7a47d0f50a0730de2e
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-07-27 10:11:16 -0500

    test: add order mocks to API mocking setup
    Created new files for mocking get-order-details and get-orders API endpoints. Integrated these mocks into the MSW setup for comprehensive testing and development.

--- src/api/mocks/index.tsx
+++ src/api/mocks/index.tsx
@@ -1,16 +1,18 @@
-import { setupWorker } from "msw/browser";
-import { env } from "../../../env.ts";
-import { signInMock } from "@/api/mocks/sign-in-mock.tsx";
-import { registerRestaurantMock } from "@/api/mocks/register-restaurant-mock.ts";
-import { getDayOrdersAmountMock } from "@/api/mocks/get-day-orders-amount-mock.ts";
-import { getMonthOrdersAmountMock } from "@/api/mocks/get-month-orders-amount-mock.ts";
-import { getMonthCanceledOrdersAmountMock } from "@/api/mocks/get-month-canceled-amount-mock.ts";
-import { getMonthRevenueMock } from "@/api/mocks/get-month-revenue-mock.ts";
 import { getDailyRevenueInPeriodMock } from "@/api/mocks/get-daily-revenue-in-period-mock.ts";
+import { getDayOrdersAmountMock } from "@/api/mocks/get-day-orders-amount-mock.ts";
+import { getManagedRestaurantMock } from "@/api/mocks/get-managed-restaurant-mock.ts";
+import { getMonthCanceledOrdersAmountMock } from "@/api/mocks/get-month-canceled-amount-mock.ts";
+import { getMonthOrdersAmountMock } from "@/api/mocks/get-month-orders-amount-mock.ts";
+import { getMonthRevenueMock } from "@/api/mocks/get-month-revenue-mock.ts";
+import { getOrderDetailsMock } from "@/api/mocks/get-order-details-mock.ts";
+import { getOrdersMock } from "@/api/mocks/get-orders-mock.ts";
 import { getPopularProductsMock } from "@/api/mocks/get-popular-products-mock.ts";
 import { getProfileMock } from "@/api/mocks/get-profile-mock.ts";
-import { getManagedRestaurantMock } from "@/api/mocks/get-managed-restaurant-mock.ts";
+import { registerRestaurantMock } from "@/api/mocks/register-restaurant-mock.ts";
+import { signInMock } from "@/api/mocks/sign-in-mock.tsx";
 import { updateProfileMock } from "@/api/mocks/update-profile-mock.ts";
+import { setupWorker } from "msw/browser";
+import { env } from "../../../env.ts";
 
 export const worker = setupWorker(
   signInMock,
@@ -24,6 +26,8 @@ export const worker = setupWorker(
   getProfileMock,
   getManagedRestaurantMock,
   updateProfileMock,
+  getOrdersMock,
+  getOrderDetailsMock,
 );
 
 export async function enableMSW() {
